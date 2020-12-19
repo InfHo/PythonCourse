b0980de3ddff528c1923a107401232e43320331d
Patrick-InfHo <patrick.haetti@infho.eu>
2020-12-19 19:02:12 +0100

    modified raute_1

--- turtle/rauten/raute_1.py
+++ turtle/rauten/raute_1.py
@@ -13,21 +13,13 @@ stift.width(2) #liniendicke
 stift.color("blue")
 
 
-def raute():
-    """
-    packe raute in Funktion (= Bauplan)
-    alles danach muss 4 leerzeichen nach rechts verschoben werden
-    """
-    stift.forward(100)
-    stift.right(60)
-    stift.forward(100)
-    stift.right(120)
-    stift.forward(100)
-    stift.right(60)
-    stift.forward(100)
-    #drehe am ende 120grad damit wieder in anfangsrichtung geschaut wird
-    #dann sind alle gradzahlen zusammengerechnet 360grad
-    stift.right(120)
-
-#führe die Funktion raute aus
-raute()
+stift.forward(100)
+stift.right(60)
+stift.forward(100)
+stift.right(120)
+stift.forward(100)
+stift.right(60)
+stift.forward(100)
+#drehe am ende 120grad damit wieder in anfangsrichtung geschaut wird
+#dann sind alle gradzahlen zusammengerechnet 360grad
+stift.right(120)
